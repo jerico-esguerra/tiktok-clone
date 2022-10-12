7082e9060247b8365bf21cada4ec9368ab0ac303
Jerico Esguerra <jericoesguerra@Jericos-MacBook-Air-2.local>
2022-10-12 17:14:48 -0400

    ...

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.boredpanda.com', 'lh3.googleusercontent.com'],
+    domains: ['www.boredpanda.com', 'lh3.googleusercontent.com', 'xsgames.co'],
   },
 }
 
